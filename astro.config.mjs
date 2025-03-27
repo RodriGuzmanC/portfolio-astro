e133b240151accb9939de734bc6c5bcefdb84d53
Rodrigo <eduis.carranza123@gmail.com>
2025-03-27 09:31:59 -0500

    correccion del deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rodriguzmanc.github.io/portfolio-astro',
+  base: '/portfolio-astro/',
+  output: 'static',
   integrations: [tailwind(), react()]
 });
